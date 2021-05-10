24ace2ba87d9d41dc9d9944f3abb1a1a777c9506
camain-elie <camain.elie@gmail.com>
2021-05-11 00:08:28 +0200

    Fixed the delete all button (was deleting every entries instead of deleting only the completed ones). Styled the menu, input, title and app.

--- src/App.js
+++ src/App.js
@@ -56,12 +56,12 @@ class App extends Component {
 
   deleteOne(index){
     const list = this.state.todoList.filter((item, i) => index !== i)
-
     this.setState({ todoList: list })
   }
 
   deleteAll(){
-    this.setState({ todoList: []})
+    const list = this.state.todoList.filter((item) => !item.completed)
+    this.setState({ todoList: list})
   }
 
   handleMenuClick(e){
@@ -71,11 +71,10 @@ class App extends Component {
   render(){
     
     const todoList = this.state.todoList.map((item, index) => {
-      //let classes = {`todo ${line.completed ? 'todo--completed' : ''} ${(this.state.currentMenu === active && line.completed ) ? 'todo--disabled' : ''}`}
 
       return(
         <li className={`todo ${item.completed ? 'todo--completed' : ''} 
-          ${(this.state.currentMenu === 'active' && item.completed ) || (this.state.currentMenu === 'completed' && !item.completed) ? 'todo--disabled' : ''}`}
+          ${(this.state.currentMenu === 'active' && item.completed ) || (this.state.currentMenu === 'completed' && !item.completed) ? 'disabled' : ''}`}
           key={index}
         >
           <input type="checkbox" checked={item.completed} onChange={() => this.handleCheckbox(index)}></input>
@@ -90,30 +89,23 @@ class App extends Component {
         <h1>#todo</h1>
         <div className="menu">
           <ul>
-            <li data-name="all" onClick={this.handleMenuClick}>All</li>
-            <li data-name="active" onClick={this.handleMenuClick}>Active</li>
-            <li data-name="completed" onClick={this.handleMenuClick}>Completed</li>
+            <li className={this.state.currentMenu === 'all' ? 'menu-active' : ''} data-name="all" onClick={this.handleMenuClick}>All</li>
+            <li className={this.state.currentMenu === 'active' ? 'menu-active' : ''} data-name="active" onClick={this.handleMenuClick}>Active</li>
+            <li className={this.state.currentMenu === 'completed' ? 'menu-active' : ''} data-name="completed" onClick={this.handleMenuClick}>Completed</li>
           </ul>
         </div>
 
-        <div className="input">
-          <input type="text" value={this.state.inputText} onChange={(event) => this.setState({ inputText: event.target.value})} />
-          <button onClick={this.addTodo}>Add</button>
+        <div className={`todo__input ${this.state.currentMenu !== 'completed' ? '' : 'disabled'}`}>
+          <input placeholder="Add details" className="todo__add-input" type="text" value={this.state.inputText} onChange={(event) => this.setState({ inputText: event.target.value})} />
+          <button className="todo__add-button" onClick={this.addTodo}>Add</button>
         </div>
-
-        
-        {this.state.currentMenu}
         
         
         <div className="todo-list">
 
           {todoList}
-
-          {/*this.state.todoList.map((line, index) => 
-            <Todo todo={line.todo} completed={line.completed} handleChange={this.handleChange} key={index} />
-          )*/}
           
-          <div className="todo-list__delete-all">
+          <div className={`todo-list__delete-all ${this.state.currentMenu === 'completed' ? '' : 'disabled'}`}>
             <button onClick={this.deleteAll}>Delete all</button>
           </div>
         </div>
