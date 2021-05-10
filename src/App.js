a142ba9ac11a7b12b8e1f8b0b000b0598040f5f6
camain-elie <camain.elie@gmail.com>
2021-05-10 18:49:31 +0200

    Working unstyled todo app with all functionalities and menu. Fixed every bug.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Component } from 'react'
 import './App.scss';
-import Todo from './Components/Todo'
 
 class App extends Component {
   constructor(props){
@@ -8,7 +7,9 @@ class App extends Component {
 
     this.addTodo = this.addTodo.bind(this)
     this.handleMenuClick = this.handleMenuClick.bind(this)
-    this.handleChange = this.handleChange.bind(this)
+    this.handleCheckbox = this.handleCheckbox.bind(this)
+    this.deleteAll = this.deleteAll.bind(this)
+    this.deleteOne = this.deleteOne.bind(this)
 
     this.state={
       todoList: [
@@ -31,7 +32,8 @@ class App extends Component {
   }
 
   addTodo(){
-    if(this.state.inputText !== ''){
+    //console.log( this.state.inputText.split(' '))
+    if(this.state.inputText){
       this.setState({
         todoList: [...this.state.todoList,{todo: this.state.inputText, completed: false}],
         inputText: '',
@@ -39,8 +41,27 @@ class App extends Component {
     }
   }
 
-  handleChange(e){
-    console.log(e.target.parentElement)
+  handleCheckbox(index){
+
+    const list = this.state.todoList.map((item, i) => {
+      if (index === i) {
+        return {todo: item.todo, completed: !item.completed}
+      } else {
+        return item
+      }
+    })
+
+    this.setState({ todoList: list })
+  }
+
+  deleteOne(index){
+    const list = this.state.todoList.filter((item, i) => index !== i)
+
+    this.setState({ todoList: list })
+  }
+
+  deleteAll(){
+    this.setState({ todoList: []})
   }
 
   handleMenuClick(e){
@@ -49,11 +70,24 @@ class App extends Component {
 
   render(){
     
+    const todoList = this.state.todoList.map((item, index) => {
+      //let classes = {`todo ${line.completed ? 'todo--completed' : ''} ${(this.state.currentMenu === active && line.completed ) ? 'todo--disabled' : ''}`}
 
+      return(
+        <li className={`todo ${item.completed ? 'todo--completed' : ''} 
+          ${(this.state.currentMenu === 'active' && item.completed ) || (this.state.currentMenu === 'completed' && !item.completed) ? 'todo--disabled' : ''}`}
+          key={index}
+        >
+          <input type="checkbox" checked={item.completed} onChange={() => this.handleCheckbox(index)}></input>
+          <p>{item.todo}</p>
+          <button onClick={() => this.deleteOne(index)}>delete me</button>
+        </li>
+      )
+    })
     
     return(
       <div className="app">
-
+        <h1>#todo</h1>
         <div className="menu">
           <ul>
             <li data-name="all" onClick={this.handleMenuClick}>All</li>
@@ -72,10 +106,18 @@ class App extends Component {
         
         
         <div className="todo-list">
-          {this.state.todoList.map((line, index) => 
+
+          {todoList}
+
+          {/*this.state.todoList.map((line, index) => 
             <Todo todo={line.todo} completed={line.completed} handleChange={this.handleChange} key={index} />
-          )}
+          )*/}
+          
+          <div className="todo-list__delete-all">
+            <button onClick={this.deleteAll}>Delete all</button>
+          </div>
         </div>
+        
       </div>
     )
   }
